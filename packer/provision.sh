cd97b83f71869e5dd1f78b2038741d714236d982
Tiansi Gu <tgu10@dons.usfca.edu>
2025-05-02 16:51:54 -0700

    Deploy containers to QA

--- packer/provision.sh
+++ packer/provision.sh
@@ -6,3 +6,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 newgrp docker
 docker --version
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
